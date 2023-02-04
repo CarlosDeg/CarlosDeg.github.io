8436b0627a43df6ed421a82073361fa0fd7f8d74
Carlos Deg <69067714+CarlosDeg@users.noreply.github.com>
2023-02-04 16:36:03 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+/*var pesoLuis = 72;
+var alturaLuis = 1.72;
+
+var pesoCarlos = 89;
+var alturaCarlos = 1.75;
+
+
+var imcCarlos;
+var imcLuis;
+//var  result;
+
+imcCarlos = pesoCarlos / (alturaCarlos * alturaCarlos);
+imcLuis = pesoLuis / (alturaLuis * alturaLuis);
+
+
+console.log("Luis"  + imcLuis + " Carlos: "+ imcCarlos);
+
+
+if( imcCarlos > imcLuis){
+
+    console.log("si es mayor" + imcCarlos);
+}else{
+
+    console("No es mayor");
+}
+
+
+//console.log(imc);
+*/
